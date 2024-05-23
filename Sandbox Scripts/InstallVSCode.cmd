1c48fb522dca7e18fae21bfb494b9bf5bb283cca
Ganesh Kakade <17514413+ganeshkakade@users.noreply.github.com>
2024-05-23 11:04:43 +0530

    Create InstallVSCode.cmd

---
+++ Sandbox Scripts/InstallVSCode.cmd
@@ -0,0 +1,5 @@
+REM Download Visual Studio Code
+curl -L "https://update.code.visualstudio.com/latest/win32-x64-user/stable" --output C:\Users\WDAGUtilityAccount\Downloads\vscode.exe
+
+REM Install and run Visual Studio Code
+C:\Users\WDAGUtilityAccount\Downloads\vscode.exe /verysilent /suppressmsgboxes
